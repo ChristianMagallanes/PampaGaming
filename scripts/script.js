07a5ec99576dfcd311d008014d7d09a56f2a6225
christian magallanes <dar4del-x@hotmail.com>
2023-10-01 23:39:15 -0300

    primer commit

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+function design() {
+  let seccion = document.getElementById("diseños");
+  seccion.scrollIntoView({ behavior: "smooth" });
+}
+function about() {
+  let seccion = document.getElementById("sobremi");
+  seccion.scrollIntoView({ behavior: "smooth" });
+}
